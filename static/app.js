9389bfbb49d735efb0083e231b4ba8de9079d1c5
Elza Hayyat <ekhayat@g.ucla.edu>
2022-04-27 19:16:08 -0700

    app.js file updated

--- static/app.js
+++ static/app.js
@@ -1,8 +1,23 @@
 console.log('l')
 d3.json("http://localhost:8000/get_data").then(function(data){
     names = []
+    volume = []
     for(let i =0 ; i < data['data'].length; i++){
         names.push(data['data'][i]['name'])
+        volume.push(data['data'][i]['market_cap_dominance'])
     }
-    console.log(names)
 })
+
+
+var data = [{
+    values: volume,
+    labels: names,
+    type: 'pie'
+  }];
+  
+  var layout = {
+    height: 400,
+    width: 500
+  };
+  
+  Plotly.newPlot('PieChart', data, layout);
